691e50d0de2058e72247734d97abf4e056262a8d
Johnathon Wood <jaywood128@gmail.com>
2018-10-22 18:33:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length >= 3
   last = array.pop
-  array.join(",") + " and " + last
+  array.join(", ") + ", and " + last
 
   end
 end
