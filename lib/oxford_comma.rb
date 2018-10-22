d64f008acde613c86d2b0b5b0e3bfce337eb1080
Johnathon Wood <jaywood128@gmail.com>
2018-10-22 17:49:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
+  elsif array.length == 3 
+    array.join
+    array.pop(3)
+    array.push("and starfruit")
   end
 end
